374bfc044181b0877b5afdb45a40ad329e2e0f2b
Shubham Malviya <you@example.com>
2023-10-22 19:44:01 +0530

    Socket events done

--- index.js
+++ index.js
@@ -4,14 +4,36 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "http://localhost:3000" }));
+const isDev = app.settings.env === "development";
+const URL = isDev ? "http://localhost:3000" : "";
+app.use(cors({ origin: URL }));
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-	cors: "http://localhost:3000",
+	cors: URL,
 });
 
 io.on("connection", (socket) => {
 	console.log("Connected to Client");
+
+	socket.on("beginPath", (arg) => {
+		socket.broadcast.emit("beginPath", arg);
+	});
+
+	socket.on("drawLine", (arg) => {
+		socket.broadcast.emit("drawLine", arg);
+	});
+
+	socket.on("drawLineCompleted", () => {
+		socket.broadcast.emit("drawLineCompleted");
+	});
+
+	socket.on("changeConfig", (arg) => {
+		socket.broadcast.emit("changeConfig", arg);
+	});
+
+	socket.on("actionClick", (arg) => {
+		socket.broadcast.emit("actionClick", arg);
+	});
 });
 
 httpServer.listen(3001);
